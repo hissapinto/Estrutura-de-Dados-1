5dae4d46ad00b9f3b40173f8d395820304db6dde
hissapinto <isabelahissap@gmail.com>
2025-02-21 12:24:30 +0000

    ex2 em andamento

---
+++ Aula1/Ex2.java
@@ -0,0 +1,13 @@
+//Escreva um código Java que leia uma letra e indique se é uma vogal ou consoante.
+
+import java.util.Scanner;
+
+public class Ex2 {
+    public static void main(String arg[]){
+        vogais = [a,e,i,o,u];
+
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("Informe uma letra: ");
+        char letra = entrada.next().charAt(0);
+    }
+}
