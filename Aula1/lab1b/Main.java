b5ba4c6207bd9fca62ab86513d2e9757932b1e9b
Isabela Hissa Pinto <isabelahissap@gmail.com>
2025-02-26 10:30:40 -0300

    Exercício palindromo finalizado

---
+++ Aula1/lab1b/Main.java
@@ -0,0 +1,37 @@
+package lab1b;
+//tem que botar isso pra linkar os códigos dessa pasta
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+
+        //Recebendo a frase
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("Escreva uma frase: ");
+        String frase = entrada.nextLine();
+
+        //Try e catch pro programa não encerrar caso haja algum erro (????)
+        try{
+            //Criando o novo obj da classe palindromo
+            Palindromo novoPalindromo = new Palindromo(frase);
+
+            //chamando a função pra verificar se é palindromo
+            boolean palindromoCorreto = novoPalindromo.verificar();
+            if(palindromoCorreto){
+                System.out.print("A frase é um palindromo");
+            }
+            else{
+                System.out.print("A frase não é um palindromo.");
+            }
+        }
+        catch(IllegalArgumentException erro){
+            System.out.println("Erro: "+erro.getMessage());
+        }
+
+        //fechando o scanner
+        entrada.close();
+    }
+}
+
+//Estou confusa com o try e catch - perguntar pro prof.
--- Aula1/lab1b/Palindromo.java
+++ Aula1/lab1b/Palindromo.java
@@ -1,28 +1,56 @@
 package lab1b;
+//tem que botar isso pra linkar os códigos dessa pasta
 
+//Criando a classe palindromo
 class Palindromo{
     private String texto;
+    //Atributo privado da classe, o texto só pode ser alterado pelos métodos da classe
+
+    //Construtor padrão
     public Palindromo(){
         this.texto = "";
     }
+    //Toda vez que um novo obj da classe for criado e não for expecificado nada pra ele
+    //ele vai ter essas características, no caso uma string vazia.
+
+    //Construtor com argumentos
     public Palindromo(String frase){
-        this.texto = frase;
+        this.setTexto(frase);
     }
-    public getTexto(){
+    //Recebe uma string(frase) e chama o metodo setTexto pra setar o texto com a frase
+
+    //Getter
+    public String getTexto(){
         return texto;
     }
-    public void setTexto(String texto){
-        if(texto == null){
-            throw new IllegalArgumentException("A frase não pode ser vazia.");
+    //Retorna a frase armazenada no obj palindromo
+
+    //Setter
+    public void setTexto(String frase){
+        if(frase == null || frase.isEmpty()){
+            throw new IllegalArgumentException("A frase não pode ser nula ou vazia.");
         }
         this.texto=frase;
     }
-    public boolean verificar(String texto){
-        int qtdLetras = texto.length;
-        palindromoCorreto = false;
-        for (int i=0;i<(qtdLetras/2);i++){
-            if()
-        }
+    //Verifica se a frase é nula, se for valida ela armazena a frase no texto
 
+    //Método pra verificar se o texto é palindromo
+    public boolean verificar(){
+        String textoFormatado = this.texto.replaceAll(" ","").toLowerCase();
+        //Tira os espaços e deixa tudo minúsculo
+        int qtdLetras = textoFormatado.length();
+        //Pega a qtd de letras
+
+        //Comparação letras opostas
+        for (int i=0;i<(qtdLetras/2);i++){
+            char letraA = textoFormatado.charAt(i);
+            char letraZ = textoFormatado.charAt(qtdLetras-i-1);
+            if(letraA != letraZ){
+                return false;
+                //Já retorna falso se achar alguma diferente
+            }
+        }
+        return true;
+        //Retorna true se não achar nada
     }
 }
